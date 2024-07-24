ac7a9df0c65a829ba32acf35fd712420d497a35f
Yash <154449658+yashveerdalal@users.noreply.github.com>
2024-07-24 10:31:11 +0530

    Add files via upload

---
+++ basic/index.js
@@ -0,0 +1,9 @@
+console.log(`helo`); //console is used for the printing 
+console.log(`is this  one on screen `);
+
+window.alert(`alert homie ^_^`)
+//window.alert() is used for posting an alert over a website 
+
+
+document.getElementById("myh1").textContent = `this is writtten by using js ^ o ^ bruhhhh `
+document.getElementById("myp").textContent =` hello, how are you doing I am currently writing this to you  by using text-content property of Java script `
