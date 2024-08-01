f405d836845d30d7abfdec0281e05752d2e6e5f3
Yash <154449658+yashveerdalal@users.noreply.github.com>
2024-08-01 10:59:23 +0530

    Add files via upload

---
+++ math/index.js
@@ -0,0 +1,48 @@
+// math = built in object that provides a collection of properties and method 
+
+
+
+console.log(Math.PI);
+let h = 4.2;
+
+let s = 5.2;
+
+console.log(Math.floor(h));
+console.log(Math.ceil(s));
+console.log(Math.trunc(s));
+
+// E is  known as eulers numbr
+
+
+console.log(Math.E);
+
+// and that is the reason why we dont use capital letter to as a variable name as varaibles
+
+let x =2.49;
+
+ let z =(Math.round(x));
+
+ // floor will always round down 
+
+ //ceil will alwys round up 
+
+ //trunc will alwys remove the decimal objects from the number 
+
+ //pow is for power 
+
+ //sqrt is for finding the square root of function 
+
+ // sin cos tan is also avalliable 
+
+ console.log(z);
+
+ console.log(Math.pow(2,3));
+
+ console.log(Math.abs(-4.5)); //abs is for mode value of data 
+
+ // max and min
+
+ let a = 2;
+ let b = 7;
+
+console.log(Math.max(a,b));
