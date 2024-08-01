13673f26c9b5637f4fca46cf470f8a6f5ddc6bab
Yash <154449658+yashveerdalal@users.noreply.github.com>
2024-08-01 10:58:05 +0530

    Add files via upload

---
+++ logical/index.js
@@ -0,0 +1,15 @@
+//    logical operators such as { and , or  ,not }
+
+//     && || ! 
+
+const isSunny = false;
+
+if(!isSunny){
+
+    console.log("it is sunny");
+
+}
+else{
+
+    console.log("it is cloudy");
+}
