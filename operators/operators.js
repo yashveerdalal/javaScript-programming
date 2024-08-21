340e0fb1dd20804443298da4d34bb801a7adb641
Yash <154449658+yashveerdalal@users.noreply.github.com>
2024-08-21 10:28:14 +0530

    Add files via upload

---
+++ operators/operators.js
@@ -0,0 +1,35 @@
+/*
+
+arithmetic operators = operands (values,varaibles , etc .)
+
+operators (+ - / * )
+
+// == compares if the values are same 
+// === compares if the value as well as the datatypes are also same 
+
+
+
+
+*/
+
+let students = 30;
+
+ console.log(students + 5);
+ console.log(students/5);
+ console.log(students/2);
+ students++;
+ console.log(students );
+ students += 4;
+ console.log(students );
+ console.log(5**2);
+ console.log(5%2);
+ // ** is for the exponent 
+
+ // % is for the remainder
+
+
+ //operators precedence applied accordingly to the order as
+ // parenthesis 
+ // exponents 
+ // multip. & div and modulo
+ // addd and sub 
